c6e73804029681a859a1dbd04828e9bb66ac07a9
kai._.mjy <kaimjy@kaist.ac.kr>
2023-07-04 11:37:21 +0900

    gallery demo 2023-07-04 11:08
    코드 정리 완료
    
    ✅ 기존에 이미지 12개 존재(인터넷 사진)
    ✅ 이미지 클릭 시 확대, 축소 가능
    ✅ '사진첩 열기' 기능을 통해 로컬 갤러리 이미지를 추가할 수 있음
    
    🧿 앞으로의 구현 사항(extra)
    📌 전체 디자인 예쁘게 변경
    📌 메모 기능 추가 / 스티커 기능?!
    📌 More EXTRA: swipe 기능 구현

--- app/src/main/java/com/example/madcampweek1/GetImageActivity.kt
+++ app/src/main/java/com/example/madcampweek1/GetImageActivity.kt
@@ -1,7 +1,6 @@
 package com.example.madcampweek1
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,9 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.madcampweek1.databinding.EditImageBinding
 import com.example.madcampweek1.ui.dashboard.DashboardFragment
-//import com.example.tap.databinding.EditImageBinding
-//import com.example.tap.databinding.FragmentDashboardBinding
-//import com.example.tap.ui.dashboard.DashboardFragment
 
 class GetImageActivity :AppCompatActivity() {
     lateinit var binding: EditImageBinding
@@ -26,22 +22,7 @@ class GetImageActivity :AppCompatActivity() {
         activityResult.launch(intent)
         //버튼 이벤트
         val dashboardfragment = DashboardFragment()
-        binding.galleryBtn.setOnClickListener {
-            //버벅 버튼을 누르면, 해당 이미지 정보를 원래 fragment_dashboard View에 전달해야함
-            //갤러리 호출
-//            val intent = Intent(Intent.ACTION_PICK)
-//            intent.type = "image/*"
-//            activityResult.launch(intent)
-            //원래 프래그먼트로 돌아가기
-//            val bundle = Bundle()
-//            bundle.putString("key", "Hello dashboard")
-//            dashboardfragment.arguments = bundle
-//
-//            val transaction = supportFragmentManager.beginTransaction()
-////            transaction.add(R.id.tv_time, dashboardfragment)
-//            transaction.commit()
-//            supportFragmentManager.beginTransaction().add(R.id.rv_board,  dashboardfragment).commit()
-        }
+
     }//onCreate
 
     //결과 가져오기
