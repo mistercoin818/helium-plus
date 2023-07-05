09608614eaa7546a601332d93d64002e1332b656
mistercoin <mistercoin@naver.com>
2023-07-05 15:22:12 +0900

    지연's commit

---
+++ app/src/main/java/com/example/madcampweek1/ViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.madcampweek1
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.madcampweek1.ui.dashboard.DashboardFragment
+import com.example.madcampweek1.ui.home.HomeFragment
+import com.example.madcampweek1.ui.weather.WeatherFragment
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount():Int = 3
+
+    override fun createFragment(position:Int): Fragment {
+        return when(position){
+            0-> HomeFragment()
+            1 -> DashboardFragment()
+            else -> WeatherFragment()
+        }
+    }
+}
